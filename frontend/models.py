0fa84a310d839429efe00a760f90b13af85f317e
franfreezy <frandelwanjawa@yahoo.com>
2024-06-06 19:30:29 +0300

    attributes set with constrains

--- frontend/models.py
+++ frontend/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class user(models.Model):
     name=models.CharField(max_length=250)
+    user_Id=models.CharField(max_length=10)
 
-    
+    def __str__(self):
+        return self.name
+        
+class 
