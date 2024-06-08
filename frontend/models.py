f1b3dc98c6ad2876448dd6d60ea3a33438318796
franfreezy <frandelwanjawa@yahoo.com>
2024-06-09 01:29:08 +0300

    tables in the database

--- frontend/models.py
+++ frontend/models.py
@@ -5,8 +5,8 @@ class user(models.Model):
     first_name=models.CharField(max_length=250)
     last_name=models.CharField(max_length=250)
     email=models.EmailField(unique=True)
-
+    #this is a table in the database
     def __str__(self):
         return f"{self.first_name} {self.first_name}"
         
-class 
+class registered(models.Model):
