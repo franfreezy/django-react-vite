9468fff5fad78c445e91a29335383fd49f7f4b40
franfreezy <frandelwanjawa@yahoo.com>
2024-06-09 01:09:34 +0300

    using formatted strings literal

--- frontend/models.py
+++ frontend/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class user(models.Model):
-    name=models.CharField(max_length=250)
-    user_Id=models.CharField(max_length=10)
+    first_name=models.CharField(max_length=250)
+    last_name=models.CharField(max_length=250)
+    email=models.EmailField(unique=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.first_name}"
         
 class 
